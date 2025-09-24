b246994f527b8a51d28c9f499860116a182fff93
Виталий Оанца <o_vital@mail.ru>
2025-09-24 18:53:35 +0300

    corrected Home Screen

--- feature/src/main/java/com/supervital/feature/screens/home/HomeScreen.kt
+++ feature/src/main/java/com/supervital/feature/screens/home/HomeScreen.kt
@@ -1,23 +1,36 @@
 package com.supervital.feature.screens.home
 
 import android.widget.Toast
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomeScreen() {
+    ColorAnimationExample()
+
+/*
     Text("Home Page Screen Content",
         fontSize = 30.sp,
         textAlign = TextAlign.Right,
         modifier = Modifier.padding(top = 15.dp)
     )
+*/
 
 /*    Surface(
         modifier = Modifier.padding(top = 40.dp),
@@ -44,6 +57,37 @@ fun HomeScreen() {
     }
 
     ToastScreen()*/
+}
+
+@Composable
+fun ColorAnimationExample() {
+    // Состояние, которое будет анимироваться
+    var isColored by remember { mutableStateOf(false) }
+
+    // Анимация для цвета
+    val animatedColor by animateColorAsState(
+        targetValue = if (isColored) Color.Red else Color.Blue,
+        label = "MyAnimatedColor" // Добавьте label для более понятного отладки
+    )
+
+    Box(
+        modifier = Modifier
+            .size(100.dp)
+            .background(animatedColor) // Применяем анимированный цвет
+    ) {
+        Button(
+            onClick = { isColored = !isColored }, // Изменяем состояние при нажатии
+            modifier = Modifier.align(Alignment.Center)
+        ) {
+            Text("Изменить цвет")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewColorAnimationExample() {
+    ColorAnimationExample()
 }
 
 @Composable
